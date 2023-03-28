2df801535f38924ddf8245caa61550a5e5f6dbf9
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-03-27 20:56:13 -0700

    Add seeds and add dield articles to query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,15 @@ module Types
     def find_article(name:)
       Article.find_by(name: name)
     end
+
+    field :articles,
+    Types::ArticleType,
+    null: false,
+    description: "Return all articles"
+
+    def articles
+      Article.all
+    end
+
   end
 end
