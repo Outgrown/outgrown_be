b7f1b47bf62a465147cc27e7a3fa400c70d29ffc
Brandon Gray <brandonjoe88@hotmail.com>
2023-04-04 12:31:41 -0600

    update: correct conflict missing data

--- spec/requests/articles/update_article_status_spec.rb
+++ spec/requests/articles/update_article_status_spec.rb
@@ -10,6 +10,37 @@ describe Types::MutationType, type: :request do
     }
   end
 
+  it 'errors on bad/missing id/user_id' do
+    expect(@article1[:user_id]).to eq(@user1[:id])
+
+    payload = {
+      "query": "mutation updateArticleStatus ($article: UpdateArticleStatusInput!) { 
+        updateArticleStatus (input: $article) { 
+          article {
+            id 
+            name 
+            status 
+            user { id name } 
+          }
+          errors
+        } 
+      }",
+      "variables": { "article": { "id": "1000", "status": "unavailable" } }
+    }
+
+    post '/graphql', headers: @headers, params: JSON.generate(payload)
+
+    article = JSON.parse(response.body, symbolize_names: true).dig(:data, :updateArticleStatus)
+
+    expect(response).to be_successful
+    expect(article).to have_key(:article)
+    expect(article[:article]).to eq(nil)
+    expect(article).to have_key(:errors)
+    expect(article[:errors]).to be_a(Array)
+    expect(article.dig(:errors, 0)).to be_a(String)
+    expect(article.dig(:errors, 0)).to eq("unable to find article")
+  end
+
   it 'updates article associated status' do
     expect(@article1[:user_id]).to eq(@user1[:id])
 
--- spec/requests/articles/update_article_user_spec.rb
+++ spec/requests/articles/update_article_user_spec.rb
@@ -11,6 +11,40 @@ describe Types::MutationType, type: :request do
     }
   end
 
+  it 'errors on bad/missing id/user_id' do
+    expect(@article1[:user_id]).to eq(@user1[:id])
+
+    payload = {
+      "query": "mutation updateArticleUser ($article: UpdateArticleUserInput!) { 
+        updateArticleUser (input: $article) { 
+          article {
+            id 
+            name 
+            user { id name } 
+          }
+          errors
+        } 
+      }",
+      "variables": { "article": { "id": "1000", "userId": "1000" } }
+    }
+
+    post '/graphql', headers: @headers, params: JSON.generate(payload)
+
+    article = JSON.parse(response.body, symbolize_names: true).dig(:data, :updateArticleUser)
+
+    binding.pry
+
+    expect(response).to be_successful
+    expect(article).to have_key(:article)
+    expect(article[:article]).to eq(nil)
+    expect(article).to have_key(:errors)
+    expect(article[:errors]).to be_a(Array)
+    expect(article.dig(:errors, 0)).to be_a(String)
+    expect(article.dig(:errors, 0)).to eq("unable to find user")
+    expect(article.dig(:errors, 1)).to be_a(String)
+    expect(article.dig(:errors, 1)).to eq("unable to find article")
+  end
+
   it 'updates article associated user_id' do
     expect(@article1[:user_id]).to eq(@user1[:id])
 
@@ -32,7 +66,7 @@ describe Types::MutationType, type: :request do
     article = JSON.parse(response.body, symbolize_names: true).dig(:data, :updateArticleUser, :article)
 
     expect(response).to be_successful
-    expect(Article.find_by(id: @article1.id)).to be_a(Article)
+    expect(Article.find_by(id: article[:id])).to be_a(Article)
     expect(article[:id]).to be_a(String)
     expect(article[:id]).to eq(@article1[:id].to_s)
     expect(article[:name]).to be_a(String)
