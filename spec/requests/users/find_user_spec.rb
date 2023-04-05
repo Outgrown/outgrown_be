d5c5fa7dd12bb60fcb383a3bab39c4e5b2f524a0
William Lampke <gravitybangers@gmail.com>
2023-04-05 12:33:58 -0400

    added sad paths to create user and find user

--- spec/requests/users/find_user_spec.rb
+++ spec/requests/users/find_user_spec.rb
@@ -24,4 +24,19 @@ describe Types::QueryType, type: :request do
         expect(user[:data][:findUser]).to have_key(:id)
         expect(user[:data][:findUser]).to have_key(:name)
     end
+    it 'errors when a user is not in the database' do
+      payload = {
+        "query": "query findUser ($id: ID!) { 
+          findUser (id: $id){ 
+            id 
+            name 
+          } 
+        }",
+        "variables":  { } }
+
+      post '/graphql', headers: @headers, params: JSON.generate(payload)
+
+      user = JSON.parse(response.body, symbolize_names: true)
+      expect(user[:errors][0][:message]).to eq("Variable $id of type ID! was provided invalid value")
+    end
 end
